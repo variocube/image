3ce3207e0136585710bd6bd4accd3f7a11d83f0e
Peter Karrer <peter@karrer.pro>
2022-10-06 15:42:20 +0200

    fix: make revpi3 image work on revpi core s

--- src/revpi3.sh
+++ src/revpi3.sh
@@ -52,9 +52,9 @@ mount "$FIRMWARE_DEV" "$FIRMWARE_DIR"
 src/bootstrap.sh "$ROOT_DIR" armhf common,raspi3,revpi3 binutils linux-image-armmp
 
 # Install and run rpi-update
-curl -L --output "$ROOT_DIR"/usr/sbin/rpi-update https://raw.githubusercontent.com/Hexxeh/rpi-update/master/rpi-update
+curl -L --output "$ROOT_DIR"/usr/sbin/rpi-update https://raw.githubusercontent.com/raspberrypi/rpi-update/master/rpi-update
 chmod +x "$ROOT_DIR"/usr/sbin/rpi-update
-chroot "$ROOT_DIR" env ROOT_PATH=/ BOOT_PATH=/boot/firmware SKIP_WARNING=1 /usr/sbin/rpi-update
+chroot "$ROOT_DIR" env ROOT_PATH=/ BOOT_PATH=/boot/firmware SKIP_WARNING=1 WANT_PI4=1 /usr/sbin/rpi-update
 
 cat >"$FIRMWARE_DIR"/cmdline.txt <<- EOF
 dwc_otg.lpm_enable=0 console=tty1 root=LABEL=ROOT rootfstype=ext4 elevator=deadline rootwait quiet
@@ -64,4 +64,9 @@ cat >"$FIRMWARE_DIR"/config.txt <<- EOF
 hdmi_drive=2
 hdmi_force_hotplug=1
 initramfs initrd.img
+
+[pi4]
+[cm4s]
+dtoverlay=dwc2,dr_mode=host
+
 EOF
