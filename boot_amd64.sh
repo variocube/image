1f126b5f0dbca8ba9c887451481eb0873cb0d03b
Peter Karrer <peter@karrer.pro>
2023-02-02 16:55:03 +0100

    chore: simplify qemu invocation

--- boot_amd64.sh
+++ boot_amd64.sh
@@ -2,6 +2,7 @@
 
 sudo qemu-system-x86_64 -smp 2 -enable-kvm -m 1024 \
   -serial stdio \
-  -netdev user,id=net0 -device e1000,netdev=net0 \
+  -nic user,model=e1000 \
   -drive file=build/amd64.img,format=raw,index=0,media=disk \
-  -boot once=c,menu=off -net user -name variocube-amd64
+  -boot once=c,menu=off \
+  -name variocube-amd64
